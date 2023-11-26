4e4147b0de7769ccf33c138455f317586f5dd504
Gatsbeau <jabriel0ne@gmail.com>
2023-11-26 09:23:21 -0500

    Added get route for homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,18 @@
-const router = require('express').Router()
-const {User, Comment, Post} = require('../models')
+const router = require("express").Router();
+const { User, Comment, Post } = require("../models");
 
-router
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [{ model: User, attributes: ["name"] }],
+    });
+
+    const allPosts = postData.map((post) => post.get({ plain: true }));
+    res.render("homepage", {
+      allPosts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
