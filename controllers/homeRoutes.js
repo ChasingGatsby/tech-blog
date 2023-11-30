156df6cfd646ded74b9a079a5fdcaf41253e6035
Gatsbeau <jabriel0ne@gmail.com>
2023-11-30 16:47:43 -0500

    Integrated update routes for posts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -62,6 +62,16 @@ router.get("/newpost", withAuth, (req, res) => {
   res.render("newpost");
 });
 
+router.get("/post/:id/update", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id)
+    const post = postData.get({ plain: true })
+    res.render("updatepost", post)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+});
+
 router.get("/login", (req, res) => {
   if (req.session.logged_in) {
     res.redirect("/dashboard");
--- public/js/updatepost.js
+++ public/js/updatepost.js
@@ -1,18 +1,18 @@
-const newPostHandler = async (event) => {
+const updatePostHandler = async (event) => {
   event.preventDefault();
-  console.log(event);
 
   const title = document.querySelector("#post-title").value;
   const content = document.querySelector("#post-content").value;
+  const postId = document.getElementById("update-btn").getAttribute("data-id");
 
   if (title && content) {
-    const response = await fetch("/api/post/", {
-      method: "POST",
+    const response = await fetch(`/api/post/${postId}`, {
+      method: "PUT",
       body: JSON.stringify({ title, content }),
       headers: { "Content-Type": "application/json" },
     });
     if (response.ok) {
-      window.location.replace("/");
+      window.location.replace(`/post/${postId}`);
     } else {
       alert(response.statusText);
     }
@@ -20,5 +20,5 @@ const newPostHandler = async (event) => {
 };
 
 document
-  .querySelector("#newpost-btn")
-  .addEventListener("click", newPostHandler);
+  .querySelector("#update-btn")
+  .addEventListener("click", updatePostHandler);
